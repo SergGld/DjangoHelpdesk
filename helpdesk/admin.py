77ddbcfcf19e5fa2c4cbd94c08103c076f957d9c
SergGld <spg4526@gmail.com>
2017-10-04 16:43:08 +0300

    admin view fix

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-# from .models import Question,Choice
 from .models import Ticket,Categories
 
-# admin.site.register(Question)
-# admin.site.register(Choice)
+
 admin.site.register(Ticket)
 admin.site.register(Categories)
 # Register your models here.
