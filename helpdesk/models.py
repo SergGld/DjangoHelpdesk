d41daab83e613123dfbc4d892475de3abfda3d54
SergGld <spg4526@gmail.com>
2017-10-04 16:44:33 +0300

    reworked CustomUser

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -8,21 +8,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 import datetime
 
-# Create your models here.
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-#     def __str__(self):
-#         return self.question_text
-#
-#     def was_published_recently(self):
-#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-#
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
+
 class Categories(models.Model):
     name=models.CharField(max_length=100)
     def __str__(self):
